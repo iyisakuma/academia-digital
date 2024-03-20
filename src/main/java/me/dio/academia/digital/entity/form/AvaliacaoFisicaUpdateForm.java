31611edd7f9f05efc3b66407ceaa308402c5139c
iyisa <yujisakuma224@gmail.com>
2024-03-20 19:22:19 -0300

    implementação de AvaliacaoFisicaServiceImpl

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaUpdateForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaUpdateForm.java
@@ -1,5 +1,12 @@
 package me.dio.academia.digital.entity.form;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AvaliacaoFisicaUpdateForm {
 
   private double peso;
--- src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
@@ -15,42 +15,49 @@ import java.util.List;
 @Service
 public class AvaliacaoFisicaServiceImpl implements IAvaliacaoFisicaService {
 
-  @Autowired
-  private AvaliacaoFisicaRepository avaliacaoFisicaRepository;
+    @Autowired
+    private AvaliacaoFisicaRepository avaliacaoFisicaRepository;
 
-  @Autowired
-  private AlunoRepository alunoRepository;
+    @Autowired
+    private AlunoRepository alunoRepository;
 
-  @Override
-  public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
-    AvaliacaoFisica avaliacaoFisica = new AvaliacaoFisica();
-    Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+    @Override
+    public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
+        AvaliacaoFisica avaliacaoFisica = new AvaliacaoFisica();
+        Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
 
-    avaliacaoFisica.setAluno(aluno);
-    avaliacaoFisica.setPeso(form.getPeso());
-    avaliacaoFisica.setAltura(form.getAltura());
+        avaliacaoFisica.setAluno(aluno);
+        avaliacaoFisica.setPeso(form.getPeso());
+        avaliacaoFisica.setAltura(form.getAltura());
 
-    return avaliacaoFisicaRepository.save(avaliacaoFisica);
-  }
+        return avaliacaoFisicaRepository.save(avaliacaoFisica);
+    }
 
-  @Override
-  public AvaliacaoFisica get(Long id) {
-    return null;
-  }
+    @Override
+    public AvaliacaoFisica get(Long id) {
+        return avaliacaoFisicaRepository.getById(id);
+    }
 
-  @Override
-  public List<AvaliacaoFisica> getAll() {
+    @Override
+    public List<AvaliacaoFisica> getAll() {
 
-    return avaliacaoFisicaRepository.findAll();
-  }
+        return avaliacaoFisicaRepository.findAll();
+    }
 
-  @Override
-  public AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate) {
-    return null;
-  }
+    @Override
+    public AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate) {
+        AvaliacaoFisica avaliacaoFisica = avaliacaoFisicaRepository.findById(id).get();
+        if (formUpdate.getPeso() != 0) {
+            avaliacaoFisica.setPeso(formUpdate.getPeso());
+        }
+        if (formUpdate.getAltura() != 0) {
+            avaliacaoFisica.setAltura(formUpdate.getAltura());
+        }
+        return avaliacaoFisicaRepository.save(avaliacaoFisica);
+    }
 
-  @Override
-  public void delete(Long id) {
+    @Override
+    public void delete(Long id) {
 
-  }
+    }
 }
